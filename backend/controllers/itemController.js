45e882239121a26ac42a83be799bab433c035565
Lucas Nazário <lucaskenpachi57@gmail.com>
2025-07-09 16:04:09 -0300

    Update itemController.js

--- backend/controllers/itemController.js
+++ backend/controllers/itemController.js
@@ -3,3 +3,8 @@ let itens = [];
 const listarItens = (req, res) => {
   res.json(itens);
 };
+
+const adiciontarItem = (req, res) => {
+  const { descricao } = req.body;
+  const novoItem = { id: item.length + 1, descricao };
+};
